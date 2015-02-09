1b92f164c597438a5783987639adf50f91d10416
Ray Xue <ray@birchpress.com>
2015-02-09 14:40:02 +0800

    Minor updates.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var slash = require('express-slash');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -6,8 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var _ = require('lodash');
-
-var app = express();
 
 var getVersion4User = function(userId) {
     var map = {
@@ -18,32 +17,16 @@ var getVersion4User = function(userId) {
     return map[userId] ? map[userId] : 'v1.0';
 }
 
-var mainRouter = express.Router();
-mainRouter.get('/:userId', function(req, res, next) {
-    var hasSlash = req.url.charAt(req.url.length - 1) === '/';
-    if(!hasSlash) {
-        res.redirect(req.url + '/');
-    } else {
-        next();
-    }
-});
-mainRouter.get('/:userId/*', function(req, res, next) {
-    var userId = req.params.userId;
-    var version = getVersion4User(userId);
-    if(req.url.indexOf('public') <= -1) {
-        req.url = '/releases/' + version + req.url;
-    }
-    next();
-});
-app.use('/', mainRouter);
+var app = express();
+// app.enable('strict routing');
 
-app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 if (app.get('env') === 'development') {
   app.use(logger('dev'));
 } else {
   app.use(logger('combined'));
 }
 
+app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -53,19 +36,33 @@ app.set('views', __dirname);
 app.set('view engine', 'hjs');
 
 var releasesDirName = 'releases';
+
+var mainRouter = express.Router();
+mainRouter.all('/:userId/*', function(req, res, next) {
+    var userId = req.params.userId;
+    var version = getVersion4User(userId);
+    if (req.url.indexOf('public') <= -1) {
+        req.url = '/' + [releasesDirName, version, req.url].join('/');
+    }
+    next();
+});
+app.use('/', mainRouter);
+
 var releasesDir = path.join(__dirname, releasesDirName);
 var releases = fs.readdirSync(releasesDir).filter(function(file) {
     return fs.statSync(path.join(releasesDir, file)).isDirectory();
 });
 _.each(releases, function(version) {
-    var virtualApp = require('./' + releasesDirName + '/' + version + '/index');
-    var prefix = releasesDirName + '/' + version;
+    var virtualApp = require(path.join(releasesDir, version, 'index'));
+    var prefix = [releasesDirName, version].join('/');
     virtualApp.setPrefix(prefix);
     var routes = virtualApp.getRoutes();
     routes.setup();
-    app.use(routes.getStaticPath(''), express.static(path.join(__dirname, releasesDirName, version, 'public')));
-    app.use('/', routes.getRouter());
+    app.use(routes.getStaticPath(''), express.static(path.join(releasesDir, version, 'public')));
+    app.use('/' + [releasesDirName, version].join('/'), routes.getRouter());
 });
+
+app.use(slash());
 
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
