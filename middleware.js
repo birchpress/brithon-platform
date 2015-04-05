aba61c1367b7890dad6a47c4bbf24c891331cf89
Luke Li <luke@birchpress.com>
2015-04-04 17:19:46 -0700

    like core, custom app supports multi plugins. App(Core) -> Plugins -> Modules; remove block functions from bundle js and copy assets code;

--- middleware.js
+++ middleware.js
@@ -5,20 +5,39 @@ var fs = require('fs');
 var _ = require('lodash');
 var director = require('director');
 var browserify = require('browserify');
-var mkdirp = require('mkdirp');
 var fse = require('fs-extra');
 var async = require('async');
 var junk = require('junk');
+var mkdirp = require('mkdirp');
 
 module.exports = function(req, res, next) {
 
-	var bundleTasks = [];
 	var requestBrithon = require('brithon-framework').newInstance();
 	req.locals.requestBrithon = requestBrithon;
 	requestBrithon.request = req;
 	requestBrithon.response = res;
 	requestBrithon.router = new director.http.Router();
 	requestBrithon.knex = req.locals.knex;
+
+	var getDefaultAppsMap = function() {
+		return {};
+	};
+
+	var getAppsMap = function(accountId) {
+		//TODO
+		//get it from DB
+		// format {
+		// 	'appointments': '1.0',
+		// 	'{appName}': '{appVersion}'
+		// }
+		if (req.locals.accountId) {
+			return {
+				'sample': 'master'
+			};
+		} else {
+			return getDefaultAppsMap();
+		}
+	};
 
 	var loadModule = function(filePath) {
 		var module = require(filePath);
@@ -30,7 +49,7 @@ module.exports = function(req, res, next) {
 		}
 	};
 
-	var loadDir = function(dirPath) {
+	var loadModules = function(dirPath) {
 		var namespaces = [];
 		var _loadDir = function(dirPath) {
 			var fileNames = fs.readdirSync(dirPath).filter(junk.not);
@@ -48,21 +67,17 @@ module.exports = function(req, res, next) {
 		return namespaces;
 	};
 
-	var getDefaultAppsMap = function() {
-		return {};
-	};
+	var bundleTasks = [];
 
-	var getAppsMap = function(accountId) {
-		//TODO
-		//get it from DB
-		return {
-			'sample': 'master'
-		};
-	};
 	var bundleJavaScript = function(src, dest) {
-		var bundle = browserify(src).bundle();
-		mkdirp.sync(path.dirname(dest));
-		var task = function(callback) {
+		bundleTasks.push(function(callback) {
+			var dirname = path.dirname(dest);
+			mkdirp(dirname, function(err) {
+				callback(err, dest);
+			});
+		});
+		bundleTasks.push(function(callback) {
+			var bundle = browserify(src).bundle();
 			var writable = fs.createWriteStream(dest);
 			bundle.on('error', function(err) {
 				callback(err, dest);
@@ -71,76 +86,92 @@ module.exports = function(req, res, next) {
 				callback(null, dest);
 			});
 			bundle.pipe(writable);
-		};
-		bundleTasks.push(task);
+		});
 	};
 
-	var getExactAppName = function(type, appName, version) {
-		if ('core' === type) {
-			return appName;
-		} else {
-			return path.join(appName, version);
-		}
+	var getPluginPublicPath = function(pluginPath) {
+		var relative = path.relative(__dirname, pluginPath);
+		return path.join(__dirname, 'public', relative);
 	};
 
-	var bundleAppJavaScripts = function(type, appName, version) {
-		var exactAppName = getExactAppName(type, appName, version);
-		var srcDir = path.join(__dirname, type, exactAppName);
+	var bundlePluginJavaScripts = function(pluginPath) {
+		var srcDir = pluginPath;
 		var fileNames = fs.readdirSync(srcDir).filter(junk.not);
 		_.forEach(fileNames, function(fileName) {
 			var filePath = path.join(srcDir, fileName);
 			if (fs.statSync(filePath).isFile()) {
-				var desPath = path.join(__dirname, 'public', type, exactAppName, fileName);
+				var pluginPublicPath = getPluginPublicPath(pluginPath);
+				var desPath = path.join(pluginPublicPath, fileName);
 				bundleJavaScript(filePath, desPath);
 			}
 		});
 	};
 
-	var copyAppAssets = function(type, appName, version) {
-		var exactAppName = getExactAppName(type, appName, version);
-		var srcDir = path.join(__dirname, type, exactAppName, 'assets');
-		var destDir = path.join(__dirname, 'public', type, exactAppName, 'assets');
-		try {
-			fse.copySync(srcDir, destDir);
-		} catch (ex) {
-			console.error(ex.message);
-		}
+	var copyAssetsTasks = [];
+
+	var copyPluginAssets = function(pluginPath) {
+		var srcDir = path.join(pluginPath, 'assets');
+		var pluginPublicPath = getPluginPublicPath(pluginPath);
+		var destDir = path.join(pluginPublicPath, 'assets');
+		var task = function(callback) {
+			fse.copy(srcDir, destDir, function(err) {
+				if (err) {
+					console.warn('Failded copying assets from ' + srcDir + ' to ' + destDir);
+					console.warn(err.message);
+				}
+				callback(null, destDir);
+			});
+		};
+		copyAssetsTasks.push(task);
 	};
 
-	function isDev() {
+	var isDev = function() {
 		return req.app.locals.config.get('env') === 'development';
 	};
 
-	var loadApp = function(type, appName, version) {
-		var exactAppName = getExactAppName(type, appName, version);
+	var loadPlugin = function(pluginPath) {
+		var serverDir = path.join(pluginPath, 'server');
+		var namespaces = [];
+		if (fs.statSync(serverDir).isDirectory()) {
+			namespaces = loadModules(serverDir);
+		}
 		if (isDev()) {
-			bundleAppJavaScripts(type, appName, version);
-			copyAppAssets(type, appName, version);
-		}
-		var appPath = path.join(__dirname, type, exactAppName, 'server');
-		var namespaces = [];
-		if (fs.statSync(appPath).isDirectory()) {
-			namespaces = loadDir(appPath);
+			bundlePluginJavaScripts(pluginPath);
+			copyPluginAssets(pluginPath);
 		}
 		return namespaces;
 	};
 
-	var loadCoreApps = function() {
+	var loadApp = function(appPath) {
 		var namespaces = [];
-		var srcDir = path.join(__dirname, 'core');
-		var appNames = fs.readdirSync(srcDir).filter(junk.not);
-		_.forEach(appNames, function(appName) {
-			var appNamespaces = loadApp('core', appName, null);
-			namespaces = namespaces.concat(appNamespaces);
-		});
+		try {
+			if (fs.statSync(appPath).isDirectory()) {
+				var pluginNames = fs.readdirSync(appPath).filter(junk.not);
+				_.forEach(pluginNames, function(pluginName) {
+					var pluginPath = path.join(appPath, pluginName);
+					if (fs.statSync(pluginPath).isDirectory()) {
+						var pluginNamespaces = loadPlugin(pluginPath);
+						namespaces = namespaces.concat(pluginNamespaces);
+					}
+				});
+			}
+		} catch (ex) {
+			console.error('Failed to load app from path ' + appPath);
+			console.error(ex.message);
+		}
 		return namespaces;
 	};
 
-	var loadApps = function(appsMap) {
+	var loadCore = function() {
+		return loadApp(path.join(__dirname, 'core'));
+	};
+
+	var loadCustomApps = function(appsMap) {
 		var namespaces = [];
 		_.forEach(appsMap, function(version, appName) {
-			var appNamespaces = loadApp('apps', appName, version);
-			namespaces = namespaces.concat(appNamespaces);
+			var appPath = path.join(__dirname, 'apps', appName, version);
+			var appNamepaces = loadApp(appPath);
+			namespaces = namespaces.concat(appNamepaces);
 		});
 		return namespaces;
 	};
@@ -149,13 +180,11 @@ module.exports = function(req, res, next) {
 		requestBrithon.core.common.server.handleErrors(err);
 	};
 
-	var coreNamepaces = loadCoreApps();
-	var appsMap = getDefaultAppsMap();
-	if (req.locals.accountId) {
-		var accountId = req.locals.accountId;
-		appsMap = getAppsMap(accountId);
-	}
-	var appNamepaces = loadApps(appsMap);
+	var coreNamepaces = loadCore();
+	var accountId = req.locals.accountId;
+	var appsMap = getAppsMap(accountId);
+	var appNamepaces = loadCustomApps(appsMap);
+
 	_.forEach(coreNamepaces, function(namespace) {
 		if (_.isFunction(namespace.init)) {
 			namespace.init();
@@ -167,13 +196,14 @@ module.exports = function(req, res, next) {
 		}
 	});
 
-	async.series(bundleTasks, function(err, results) {
+	var allTasks = [].concat(bundleTasks).concat(copyAssetsTasks);
+
+	async.series(allTasks, function(err, results) {
 		if (err) {
-			console.error(err);
-		} else {
-			requestBrithon.router.dispatch(req, res, function(err) {
-				handleErrors(req, res, err);
-			});
+			console.error(err.message);
 		}
+		requestBrithon.router.dispatch(req, res, function(err) {
+			handleErrors(req, res, err);
+		});
 	});
 };
